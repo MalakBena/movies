202962eba3d2ae4e19a5498a7e255f699384bbe9
MalakBena <malakmaab@hotmail.co.uk>
2023-02-27 13:52:03 +0000

    implements task 2 part 2

--- db/02-further-queries.sql
+++ db/02-further-queries.sql
@@ -2,8 +2,24 @@
 
 \echo '\n 1. Here are the number of films in stock for each genre:'
 
+SELECT genre_name, COUNT(genre_name) AS number_in_stock
+FROM stock
+JOIN movies_genres
+ON movies_genres.movie_id = stock.movie_id
+JOIN genres
+ON genres.genre_id = movies_genres.genre_id
+GROUP BY genre_name;
+
 
 \echo '\n 2. Here is the average rating for films in stock in Newcastle:'
+
+SELECT AVG(ROUND(rating))  AS average_rating_newcastle
+FROM movies
+JOIN stock
+ON movies.movie_id = stock.movie_id
+JOIN stores
+ON stores.store_id = stock.store_id
+WHERE city = 'Newcastle';
 
 
 \echo '\n 3. Here are all the films made in 90s with above average rating:'
@@ -22,4 +38,3 @@
 
 
 \echo '\n 7b. ...and this is the most abundant genre in that store:'
-
